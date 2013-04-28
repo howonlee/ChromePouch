550a53cf92ed9d106ad79ac1403440ed87ed57d3
Howon Lee <hlee.howon@gmail.com>
2013-04-28 12:57:25 -0700

    getting back from content script

--- content.js
+++ content.js
@@ -3,9 +3,26 @@
 //content scripts are run in an isolated environment, 
 //so we can fool around however we want with globals and such
 
-console.log("begin content script");
-var checkboxes = $(':checkbox');
-console.log("length");
-console.log(checkboxes.length);
-var arr = $.makeArray(checkboxes);
-console.log(arr);
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){	
+	console.log("message listener in content script popped");
+	console.log(message);
+	var command = message.command;
+	var name = message.name || 'hello';
+	switch (command){
+		case 'checkbox':
+			console.log("command checkbox popped");
+			var checkboxes = $(':checkbox');
+			console.log("length");
+			console.log(checkboxes.length);
+			var arr = $.makeArray(checkboxes);
+			var vals = $.map(arr, function(elem, index){
+				return elem.checked;
+			});
+			console.log(vals);
+			responseMessage = {	name: name, vals: vals,type: "content" };
+			chrome.runtime.sendMessage(responseMessage, function(resp){});
+			break;
+		default:
+			break;
+	}
+});
--- event.js
+++ event.js
@@ -1,27 +1,42 @@
 
+var lastTabId = -1;
+function sendMessage(){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		lastTabId = tabs[0].id;
+		chrome.tabs.sendMessage(lastTabId, {command: "checkbox", name: "walla"});
+	});
+}
+
 chrome.browserAction.onClicked.addListener(function(){
 	console.log("browseraction clicked");
 	chrome.tabs.executeScript(null, {
 		file: "./jquery-1.9.1.min.js"
+	}, function(){
+		chrome.tabs.executeScript(null, {file: "./content.js"}, function(){
+			sendMessage();
+		});
 	});
-	chrome.tabs.executeScript(null, {file: "./content.js"});
 	var iframe = document.getElementById('ourFrame');
-	var message = {
-		command: 'save',
-		context: {thing: 'world'}
-	};
+	var message = {	command: 'save', context: {thing: 'world'}};
 	iframe.contentWindow.postMessage(message, '*')
+});
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+	console.log("got back from content script");
+	console.log(message);
 });
 
 window.addEventListener('message', function(event){
 	console.log("eventlistener popped");
 	console.log(event);
-	if (event.data.html){
+	if (event.data.type === "sandbox"){
 		var notification = webkitNotifications.createNotification(
 			'icon.png',
 			'Templated!',
 			event.data.html
 			);
 		notification.show();
+	} else {
+		console.log(event.data);
 	}
 });
