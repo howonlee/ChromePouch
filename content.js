4d9ca350430357ecde39802a5c9e5535d94c8098
Howon Lee <hlee.howon@gmail.com>
2013-04-28 14:09:41 -0700

    factor out browser action messiness

--- content.js
+++ content.js
@@ -7,19 +7,28 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 	console.log("message listener in content script popped");
 	console.log(message);
 	var command = message.command;
-	var name = message.name || 'hello';
 	switch (command){
-		case 'checkbox':
-			console.log("command checkbox popped");
+		case 'getcheckbox':
+			console.log("command getcheckbox popped");
+			var name = message.name || 'hello';
 			var checkboxes = $(':checkbox');
-			console.log("length");
-			console.log(checkboxes.length);
 			var arr = $.makeArray(checkboxes);
 			var vals = $.map(arr, function(elem, index){
 				return elem.checked;
 			});
 			console.log(vals);
 			responseMessage = {	name: name, vals: vals,type: "content" };
+			chrome.runtime.sendMessage(responseMessage, function(resp){});
+			break;
+		case 'setcheckbox':
+			console.log("command setcheckbox popped");
+			var checkboxes = $(':checkbox');
+			var data = message.data;
+			var arr = $.makeArray(checkboxes);
+			for (var i = 0; i < arr.length; i++){
+				arr[i].checked = data.vals[i];
+			}
+			responseMessage = {	ok: true };
 			chrome.runtime.sendMessage(responseMessage, function(resp){});
 			break;
 		default:
--- event.js
+++ event.js
@@ -1,29 +1,62 @@
 
 var lastTabId = -1;
-function sendMessage(){
+var state = "save";
+var data = {};
+function sendSaveMessage(){
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		lastTabId = tabs[0].id;
-		chrome.tabs.sendMessage(lastTabId, {command: "checkbox", name: "walla"});
+		chrome.tabs.sendMessage(lastTabId, {command: "getcheckbox", name: "walla"});
 	});
 }
 
-chrome.browserAction.onClicked.addListener(function(){
-	console.log("browseraction clicked");
+function sendLoadMessage(){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		lastTabId = tabs[0].id;
+		chrome.tabs.sendMessage(lastTabId, {command: "setcheckbox", data: data});
+	});
+}
+
+function save(){
+	console.log("save browseraction clicked");
 	chrome.tabs.executeScript(null, {
 		file: "./jquery-1.9.1.min.js"
 	}, function(){
 		chrome.tabs.executeScript(null, {file: "./content.js"}, function(){
-			sendMessage();
+			sendSaveMessage();
 		});
 	});
-	var iframe = document.getElementById('ourFrame');
-	var message = {	command: 'save', context: {thing: 'world'}};
-	iframe.contentWindow.postMessage(message, '*')
+}
+
+function load(){
+	console.log("load browseraction clicked");
+	chrome.tabs.executeScript(null, {
+		file: "./jquery-1.9.1.min.js"
+	}, function(){
+		chrome.tabs.executeScript(null, {file: "./content.js"}, function(){
+			sendLoadMessage();
+		});
+	});
+}
+
+chrome.browserAction.onClicked.addListener(function(){
+	if (state === "save"){
+		save();
+		state = "load";
+	} else if (state === "load") {
+		load();
+		state = "save";
+	}
 });
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 	console.log("got back from content script");
 	console.log(message);
+	if (message.type === "content"){
+		var iframe = document.getElementById('ourFrame');
+		data = message;
+		var toSave = {	command: 'save', context: message};
+		iframe.contentWindow.postMessage(toSave, '*');
+	}
 });
 
 window.addEventListener('message', function(event){
@@ -32,7 +65,7 @@ window.addEventListener('message', function(event){
 	if (event.data.type === "sandbox"){
 		var notification = webkitNotifications.createNotification(
 			'icon.png',
-			'Templated!',
+			'Saved!',
 			event.data.html
 			);
 		notification.show();
