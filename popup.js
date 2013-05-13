270e125a5c8511f63907ae78d86d08e16d13157b
Howon Lee <hlee.howon@gmail.com>
2013-05-12 22:37:22 -0700

    added localstorage of urls

--- popup.js
+++ popup.js
@@ -1,15 +1,44 @@
 $(document).ready(function(){
   var bg = chrome.extension.getBackgroundPage();
+
+  function save_url(){
+    localstorage["chrome_pouch_remote_url"] = $("#replto").val();
+    localstorage["chrome_pouch_local_url"] = $("#replfrom").val();
+    var stat = $("#status");
+    stat.html("Options saved");
+    setTimeout(function(){
+      stat.html("");
+    }, 1000);
+  }
+
+  function restore_url(){
+    var remoteurl = localstorage["chrome_pouch_remote_url"];
+    var localurl = localstorage["chrome_pouch_local_url"];
+    if (!remoteurl){
+      remoteurl = "";
+    }
+    if (!localurl){
+      localurl = "";
+    }
+    var remote = $("#replto");
+    remote.val(remoteurl);
+    var local = $("#replfrom");
+    local.val(localurl);
+  }
+
   function replTo(){
+    save_url();
     var text = $("replto").val();
     bg.replTo(text);
   }
 
   function replFrom(){
+    save_url();
     var text = $("replfrom").val();
     bg.replFrom(text);
   }
 
+  restore_url();
   $("#repltobutton").click(replTo);
   $("#replfrombutton").click(replFrom);
   $("#savebutton").click(bg.saveButtonCallback);
